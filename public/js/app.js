3be4c96def382400ad0b8a0786326a38b04c179e
cherryzcheerz <cherry.9nine@gmail.com>
2017-06-24 10:30:18 +0700

    hello

--- public/js/app.js
+++ public/js/app.js
@@ -20,3 +20,17 @@
 	registerServiceWorker();
 	
 })(window, navigator);
+
+$(".button-collapse").sideNav();
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+$('.modal').modal();
+$('input.autocomplete').autocomplete({
+  data: {"Apple": null, "Microsoft": null, "Google": 'http://placehold.it/250x250'},
+});
+});
+
+$('.datepicker').pickadate({
+selectMonths: true, // Creates a dropdown to control month
+selectYears: 15 // Creates a dropdown of 15 years to control year
+});
