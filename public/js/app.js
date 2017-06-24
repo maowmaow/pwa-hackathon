a599fe8a3a75229d740a80df761dad7a133e00c4
CandyAou <paspana.assarasee@gmail.com>
2017-06-24 15:10:08 +0700

    Implement Chat feature

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,22 @@
 
 (function(window, navigator) {
 	
-	function registerServiceWorker() {
-		console.log('begin: register service worker');
-		if ('serviceWorker' in navigator) {
-		  window.addEventListener('load', function() {
-		    navigator.serviceWorker
-		    	.register('/service-worker.js')
-		    	.then(function(registration) {
-		    		console.log('ServiceWorker registration successful with scope: ', registration.scope);
-		    	}, function(err) {
-		    		console.log('ServiceWorker registration failed: ', err);
-		    	});
-		  });
-		}
-	}
+	// function registerServiceWorker() {
+	// 	console.log('begin: register service worker');
+	// 	if ('serviceWorker' in navigator) {
+	// 	  window.addEventListener('load', function() {
+	// 	    navigator.serviceWorker
+	// 	    	.register('/service-worker.js')
+	// 	    	.then(function(registration) {
+	// 	    		console.log('ServiceWorker registration successful with scope: ', registration.scope);
+	// 	    	}, function(err) {
+	// 	    		console.log('ServiceWorker registration failed: ', err);
+	// 	    	});
+	// 	  });
+	// 	}
+	// }
 	
-	registerServiceWorker();
+	//registerServiceWorker();
 	
 })(window, navigator);
 
